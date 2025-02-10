efcf12499a6c24528086911df03ce1d8108b2a1e
Yevhen <yevhen.yatskiv@gmail.com>
2025-02-10 16:59:36 +0100

    Added simple form

---
+++ formik/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import YoutubeForm from './components/YoutubeForm'
+
+function App() {
+
+  return (
+    <YoutubeForm />
+  )
+}
+
+export default App
---
+++ formik/src/components/YoutubeForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useFormik } from 'formik';
+
+function YoutubeForm() {
+  const formik = useFormik({});
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" name="channel" />
+
+        <p>
+          <button>Submit</button>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
